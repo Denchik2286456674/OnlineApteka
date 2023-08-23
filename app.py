2ca66679cae46b036b66faeb7a67d13eefdfc608
Денис Морощук <115397097+Denchik2286456674@users.noreply.github.com>
2023-08-23 14:48:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
-from sql_queries import ShopDB
+from sql_queries import AptekaDB
 
 app = Flask(__name__)
-db = ShopDB()
-
+db = AptekaDB()
 
 @app.route("/")
 def index():
     items = db.get_all_items()
 
-    return render_template("index.html, items=items")
+    return render_template("index.html", item=item)
 
-if __name__ =="main":
+@app.route("/item/<item_id>")
+def item(item_id):
+    item = db.get_items(item_id)
+
+    return render_template("item.html", item=item)
+
+@app.route("/order/<item_id>", methods=["GET", "POST"])
+def order(item_id):
+    item = db.get_item(item_id)
+
+    return render_template("order.html", item=item)
+
+if __name__ =="__main__":
     app.config['TAMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
---
+++ sql_queries.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class AptekaDB:
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+        self.db_name = "apteka.db"
+ 
+    def open(self):
+        self.conn =sqlite3.connect("apteka.db")
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def get_all_items(self):
+        self.open()
+        self.cursor.execute("SELECT * FROM items")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+
+    def get_item(self, id):
+        self.open()
+        self.cursor.execute("SELECT * FROM items WHERE id==(?)", [id])
+        data = self.cursor.fetchone()
+        self.close()
+        return data
